bfca98c96a4af8a7b5e954f368bf12ad64699de9
Ghaida Alharthi <107882994+GhaidaAlharth@users.noreply.github.com>
2024-01-17 05:39:13 +0300

    Update telephone_directory.py

--- telephone_directory.py
+++ telephone_directory.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 phone_book = {
     'Amal': '1111111111',
@@ -61,10 +56,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# In[ ]:
-
-
-
-
